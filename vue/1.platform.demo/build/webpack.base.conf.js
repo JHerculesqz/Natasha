7ec4f54e8cf234a77bcd6d7c1de5f36defc6a2ba
JHercules <JHercules_qz@qq.com>
2019-07-20 18:14:58 +0800

    Natasha:update Project

--- vue/1.platform.demo/build/webpack.base.conf.js
+++ vue/1.platform.demo/build/webpack.base.conf.js
@@ -27,7 +27,7 @@ module.exports = {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
       'jquery':'jquery',
-      '~~': "core-fw-vue-widget-new/src/components/walle",
+      '~~': "natasha-vue/src/walle",
     }
   },
   module: {
