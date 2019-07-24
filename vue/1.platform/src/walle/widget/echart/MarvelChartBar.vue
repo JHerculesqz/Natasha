b9b357d7b48fe4238dfdd89773e94f50b1ce44d5
DESKTOP-F6FT34E\gerti <gertei201008>
2019-07-24 20:19:25 +0800

    Revert "控件同步"
    This reverts commit 6a157789a9fe2c7b7b3a965ada92f88e8d080aa1.

--- vue/1.platform/src/walle/widget/echart/MarvelChartBar.vue
+++ vue/1.platform/src/walle/widget/echart/MarvelChartBar.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="chart" v-bind:id="id"></div>
+  <div class="chart" v-bind:id="id"
+       v-bind:style="{width: width + 'px', height: height + 'px'}"></div>
 </template>
 
 <script>
@@ -12,18 +13,33 @@
   require('echarts/lib/component/dataZoom');
 
   /**
-   *  MarvelChartBarLine widget description
+   *  MarvelChartBar widget description
    *  @vuedoc
-   *  @exports MarvelChartBarLine
+   *  @exports MarvelChartBar
    */
   export default {
-    name: 'MarvelChartBarLine',
+    name: 'MarvelChartBar',
     props: {
       id: {
         type: String,
         default: "",
         required: true,
       },
+      theme: {
+        type: String,
+        default: "",
+        required: false,
+      },
+      width: {
+        type: Number,
+        default: undefined,
+        required: false,
+      },
+      height: {
+        type: Number,
+        default: undefined,
+        required: false,
+      }
     },
     data: function () {
       return {
@@ -31,17 +47,17 @@
         chartData: undefined
       }
     },
-    beforeDestroy: function () {
-      //#region destroy
-
-      this._destroy();
-
-      //#endregion
-    },
     mounted: function () {
       //#region init
 
       this._initEx();
+
+      //#endregion
+    },
+    beforeDestroy: function () {
+      //#region destroy
+
+      this._destroy();
 
       //#endregion
     },
@@ -53,9 +69,20 @@
       _initEx: function () {
         var self = this;
 
-        this.chartObj = echarts.init(document.getElementById(this.id));
+        this.chartObj = echarts.init(document.getElementById(this.id), this.theme);
         this.chartObj.on("click", function (params) {
-          self.callback4OnBarItemClick(params);
+          if (params.componentType == "series" &&
+            params.componentSubType == "bar") {
+
+            var oBuObj = undefined;
+            if (self.chartData.lstBuObj != undefined) {
+              if (params.dataIndex <= self.chartData.lstBuObj.length - 1) {
+                oBuObj = self.chartData.lstBuObj[params.dataIndex];
+              }
+            }
+
+            self.callback4OnBarItemClick(params, oBuObj);
+          }
         });
       },
       _destroy: function () {
@@ -64,118 +91,11 @@
 
       //#endregion
 
-      generateZoom: function (bIsZoom) {
-        if (bIsZoom) {
-          if (this.chartData.sliderConfig != undefined) {
-            //用于控制一次展示的柱子的百分比以及滑动块离底部距离
-            return [{
-              type: 'slider',
-              fillerColor: '#687178',
-              height: 10,//设置滑动条高度
-              handleSize: 20,
-              start: this.chartData.sliderConfig.start,//滑动块展示起始的百分比
-              end: this.chartData.sliderConfig.end,//滑动块展示结束的百分比
-              bottom: this.chartData.sliderConfig.end,//滑动块展示离底部距离
-              showDetail: false,
-              zoomLock: true,
-            }];
-          } else {
-            return [{
-              type: 'slider',
-              fillerColor: '#687178',
-              height: 10,//设置滑动条高度
-              handleSize: 20,
-              start: 0,
-              end: 50,
-              bottom: 10,
-              showDetail: false,
-              zoomLock: true,
-            }]
-          }
-        } else {
-          return [];
-        }
-      },
-      generateYaxis: function (arrYAxis, arrSeries, isReCalc) {
-        var arrYaxis = [];
-        if (isReCalc != undefined && isReCalc == true) {
-          var maxValue = 0;
-          for (var j = 0; j < arrSeries.length; j++) {
-            for (var k = 0; k < arrSeries[j].data.length; k++) {
-              if (maxValue < parseFloat(arrSeries[j].data[k])) {
-                maxValue = parseFloat(arrSeries[j].data[k])
-              }
-            }
-          }
-
-          maxValue = maxValue + 1;
-          var n = 0;
-          var m = Math.floor(maxValue);
-          while (m != 0) {
-            m = Math.floor(m / 10);
-            n++;
-          }
-          if (Math.floor(maxValue) < 5) {
-            maxValue = 5;
-          } else {
-            maxValue = 10;
-          }
-        } else if (maxValue > Math.floor(Math.floor(maxValue) / Math.pow(10, n - 1)) * Math.pow(10, n - 1) && maxValue <= Math.floor(Math.floor(maxValue) / Math.pow(10, n - 1)) * Math.pow(10, n - 1) + 5 * Math.pow(10, n - 2)) {
-          maxValue = Math.floor(Math.floor(maxValue) / Math.pow(10, n - 1)) * Math.pow(10, n - 1) + 5 * Math.pow(10, n - 2);
-        } else if (maxValue > Math.floor(Math.floor(maxValue) / Math.pow(10, n - 1)) * Math.pow(10, n - 1) + 5 * Math.pow(10, n - 2)) {
-          maxValue = Math.floor(Math.floor(maxValue) / Math.pow(10, n - 1)) * Math.pow(10, n - 1) + Math.pow(10, n - 1);
-        }
-
-        for (var i = 0; i < arrYAxis.length; i++) {
-          var oYaxis = {
-            type: '',
-            name: arrYAxis[i].title,
-            show: arrYAxis[i].isShow,
-            splitLine: {
-              lineStyle: {
-                type: 'dash',//控制横向的虚线分割线
-              }
-            },
-            min: 0,
-            max: maxValue,
-            interval: maxValue / 5,
-          };
-          arrYaxis.push(oYaxis);
-        }
-        return arrYaxis;
-      },
-      genToolTip: function (oData) {
-        if (oData.customerTip) {
-          return {
-            trigger: 'axis',
-            axisPointer: {
-              type: 'cross',
-              crossStyle: {
-                color: '#999'
-              }
-            },
-            formatter: function () {
-              return oData.tip[prarm[0].dataIndex];
-            }
-          }
-        } else {
-          return {
-            trigger: 'axis',
-            axisPointer: {
-              type: 'cross',
-              crossStyle: {
-                color: '#999'
-              }
-            }
-          }
-        }
-      },
-
       //#endregion
       //#region callback
 
-      callback4OnItemClick: function (params) {
-        this.$emit("onItemClick", params);
+      callback4OnBarItemClick: function (params, oBuObj) {
+        this.$emit("onBarItemClick", params, oBuObj);
       },
 
       //#endregion
@@ -184,38 +104,31 @@
       setData: function (oData) {
         this.chartData = oData;
         var option = {
-          color: this.chartData.colors,
-          tooltip: this.genToolTip(oData),
-          dataZoom: this.generateZoom(this.chartData.isSlide),
-          legend: {
-            data: this.chartData.category,
-            selectedMode: this.chartData.canSelect == undefined ? true : this.chartData.canSelect,
+          title: {
+            text: oData.title
           },
-          xAxis: [
-            {
-              type: 'category',
-              name: this.chartData.xAxisName,
-              data: this.chartData.xAxisValue,//横坐标的值
-              axisPointer: {
-                type: 'shadow'
-              }
-            }
-          ],
-          yAxis: this.generateYaxis(this.chartData.yAxis, this.chartData.series, this.chartData.isReCalc),
-          series: this.chartData.series
+          tooltip: {
+            trigger: 'item',
+            formatter: "{b} : {c}"
+          },
+          dataZoom: [{
+            type: 'slider',
+            start: 0,
+            end: 100
+          }],
+          xAxis: {
+            type: 'category',
+            data: oData.xData,
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [{
+            data: oData.yData,
+            type: 'bar'
+          }]
         };
-        if (this.chartData.top != undefined) {
-          option['grid'] = {
-            top: this.chartData.top
-          };
-        }
-        if (this.chartData.axisLabel != undefined) {
-          option['xAxis'][0].axisLabel = this.chartData.axisLabel;
-        }
         this.chartObj.setOption(option, true);
-      },
-      resize: function () {
-        this.chartObj.resize();
       }
 
       //#endregion
