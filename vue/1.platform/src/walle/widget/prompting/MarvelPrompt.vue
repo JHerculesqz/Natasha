6311e852b621e9e088a1d7c0f4c5754ba8ba82b3
DESKTOP-F6FT34E\gerti <gertei201008>
2019-09-11 12:00:56 +0800

    控件合并、更新

--- vue/1.platform/src/walle/widget/prompting/MarvelPrompt.vue
+++ vue/1.platform/src/walle/widget/prompting/MarvelPrompt.vue
@@ -1,49 +1,40 @@
 <template>
-  <div class="promptContainer" :class="classObj">
-    <div class="left" :class="leftIconClass"></div>
-    <div class="center" v-html="content">
-    </div>
-    <div class="right icon-cross" @click="clickDel"></div>
+  <div class="promptGlobalContainer" v-dom-portal="isGlobal">
+    <!--<div class="promptBoxes" >-->
+    <marvel-prompt-item class="customStyle" v-for="(prompt, index) in oPrompts"
+                  :key="prompt.id ? prompt.id : index"
+                  :status="prompt.status"
+                  :content="prompt.content"
+                  :prompt="prompt"
+                  @destroy="promptDestroy"
+                  :disposeTimeOut="disposeTimeOut"></marvel-prompt-item>
+    <!--</div>-->
   </div>
 </template>
 
 <script>
-  import brower from '../../component/browser';
+  import MarvelPromptItem from './MarvelPromptItem.vue';
 
   /**
-   *  MarvelPrompt widget description
+   *  MarvelPromptGlobal widget description
    *  @vuedoc
-   *  @exports MarvelPrompt
+   *  @exports MarvelPromptGlobal
    */
   export default {
-    name: "MarvelPrompt",
-    data: function () {
-      return {
-        timeId: undefined,
-      };
+    components: {
+      MarvelPromptItem
     },
+    name: "MarvelPromptGlobal",
     props: {
-      status: {//status: '0'表示info,'1'代表提示,'2'表示警告
-        type: String,
-        default: "2",
-        required: false,
-        validator: function (str) {
-          if (str == "0" || str == "1" || str == "2") {
-            return true;
-          } else {
-            return false;
-          }
-        }
-      },
-      content: {
-        type: String,
-        default: "",
+      isGlobal: {
+        type: Boolean,
+        default: false,
         required: false,
       },
-      prompt: {
-        type: [String, Object, Array],
+      oPrompts: {
+        type: Array,
         default: undefined,
-        required: false,
+        required: true,
       },
       disposeTimeOut: {
         type: Number,
@@ -51,82 +42,20 @@
         required: false,
       }
     },
-    mounted: function () {
-      //#region init
-
-      this._initEx();
-
-      //#endregion
-    },
-    beforeDestroy() {
-      //#region destroy
-
-      this._destroy();
-
-      //#endregion
-    },
-    computed: {
-      classObj: function () {
-        if (this.status == '0') {
-          return 'promptInfo';
-        }
-        if (this.status == '1') {
-          return 'promptTip';
-        } else {
-          return 'promptError';
-        }
-      },
-      leftIconClass: function () {
-        if (this.status == '0') {
-          return ["icon-checkmark", "leftInfo"];
-        } else if (this.status == '1') {
-          return ["icon-info", "leftTip"];
-        } else {
-          return ["icon-info", "leftError"];
-        }
-      }
+    data: function () {
+      return {};
     },
     methods: {
       //#region inner
-
-      //#region lifeCycle
-
-      _initEx: function () {
-        this.timeId = setTimeout(function () {
-          this.clickDel();
-        }.bind(this), this.disposeTimeOut);
-      },
-      _destroy: function () {
-        if (this.timeId) {
-          clearTimeout(this.timeId);
-        }
-        this.callback4Destroy(this.prompt);
-      },
-
-      //#endregion
-
       //#endregion
       //#region callback
 
-      callback4Destroy: function (oPrompt) {
-        this.$emit("destroy", oPrompt);
-      },
+      promptDestroy(prompt) {
+        this.$emit("promptDestroy", prompt);
+      }
 
       //#endregion
       //#region 3rd
-
-      clickDel: function () {
-        var oBrowser = brower.getBrowserInfo();
-        if (oBrowser.name == 'msie') {
-          //iE 浏览器下
-          this.$el.removeNode(true);
-        } else {
-          //非IE
-          this.$el.remove();
-        }
-        this.$destroy();
-      }
-
       //#endregion
     },
   }
@@ -154,75 +83,14 @@
   }
 
   /*endregion*/
-  .promptContainer {
-    width: 400px;
-    min-height: 40px;
-    box-sizing: border-box;
-    padding: 0 15px;
-    font-size: 14px;
-    border: 1px solid;
-    border-radius: 6px;
-    overflow: hidden;
+  .promptGlobalContainer {
+    position: fixed;
+    bottom: 0px;
+    right: 20px;
+    z-index: 3003;
   }
 
-  .promptInfo {
-    background-color: rgba(61, 204, 166, 0.3);
-    border-color: #5ccbb1;
-    color: #5ccbb1;
-    font-size: 16px;
-  }
-
-  .promptTip {
-    background-color: rgba(51, 153, 255, 0.3);
-    border-color: #1b8adb;
-    color: #1b8adb;
-    font-size: 16px;
-  }
-
-  .promptError {
-    background-color: rgba(255, 76, 76, 0.3);
-    border-color: #e54545;
-    color: #e54545;
-    font-size: 16px;
-  }
-
-  .promptContainer .left {
-    float: left;
-    margin-top: 12px;
-    width: 16px;
-    height: 16px;
-    border-radius: 50%;
-    color: #FFFFFF;
-    line-height: 16px;
-    font-size: 8px;
-    text-align: center;
-  }
-
-  .leftInfo {
-    background-color: #3dcca6;
-  }
-
-  .leftTip {
-    background-color: #3399ff;
-  }
-
-  .leftError {
-    background-color: #ff4c4c;
-  }
-
-  .promptContainer .center {
-    float: left;
-    padding: 10px 15px 10px 10px;
-    box-sizing: border-box;
-    max-width: calc(100% - 36px);
-  }
-
-  .promptContainer .right {
-    float: right;
-    margin-top: 12px;
-    line-height: 16px;
-    font-size: 8px;
-    text-align: center;
-    width: 12px;
+  .customStyle {
+    margin-bottom: 10px;
   }
 </style>
